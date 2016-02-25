040c0a1dfbd50c23ab31518d9d31d29988bf6237
aylestoneallotments <newsletter@aylestoneallotments.co.uk>
2016-02-25 22:09:04 +0000

    Merge in event hide

---
+++ _javascript/events-hide.js
@@ -0,0 +1,10 @@
+(function() {
+		$.getJSON("events.json", function(data) {
+				var items = [];
+				$.each(data.events, function(key, val) {
+						if (Date.parse(data.events[key].start) > $.now()) {
+								$(".event-" + (key + 1) ).show();
+							}
+						});
+				});
+		})();
