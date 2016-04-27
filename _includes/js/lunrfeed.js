eb5c76d6a1594d5edeb9627a956748ea1b8bc0ad
aylestoneallotments <newsletter@aylestoneallotments.co.uk>
2016-04-27 22:29:02 +0100

    Fix search

--- _includes/js/lunrfeed.js
+++ _includes/js/lunrfeed.js
@@ -10,19 +10,19 @@ index.add({
   title: {{post.title | jsonify}},
   content: {{post.content | strip_html | jsonify}},
   id: {{count}}
-});{% endif %}
-{% assign count = count | plus: 1 %}{% endfor %}
+});{% assign count = count | plus: 1 %}{% endif %}
+{% endfor %}
 {% for page in site.pages %}
   {% if page.title != null %}
 index.add({
   title: {{page.title | jsonify}},
   summary: {{page.summary | jsonify}},
   id: {{count}}
-});{% endif %}
-{% assign count = count | plus: 1 %}{% endfor %}
+});{% assign count = count | plus: 1 %}{% endif %}
+{% endfor %}
 // builds reference data
-var store = [{% for post in site.posts %}{
-  {% if post.title != null %}
+var store = [{% for post in site.posts %}
+  {% if post.title != null %}{
   "title": {{post.title | jsonify}},
   "link": {{ site.baseurl | | append: post.url | jsonify }},
   "date": {{ post.date | date: '%-d %B %Y' | jsonify }},
@@ -31,10 +31,10 @@ var store = [{% for post in site.posts %}{
    {% else %}
    "summary": {{ post.content | strip_html | truncatewords: 20 | jsonify }},
    {% endif %}
-   {% endif %}
-}{% unless forloop.last %},{% endunless %}{% endfor %},
-{% for page in site.pages %}{
-  {% if page.title != null %}
+   }{% unless forloop.last %},{% endunless %}{% endif %}
+{% endfor %},
+{% for page in site.pages %}
+  {% if page.title != null %}{
   "title": {{page.title | jsonify}},
   "link": {{ site.baseurl | | append: page.url | jsonify }},
   {% if page.summary != null %} 
@@ -42,8 +42,8 @@ var store = [{% for post in site.posts %}{
   {% else %}
   "summary": {{ page.content | strip_html | truncatewords: 20 | jsonify }},
   {% endif %}
-  {% endif %}
-}{% unless forloop.last %},{% endunless %}{% endfor %}
+  }{% unless forloop.last %},{% endunless %}{% endif %}
+{% endfor %}
 ]
 // builds search
 $(document).ready(function() {
