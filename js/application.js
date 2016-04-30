cda2fcaf653c6f5462e14714afb5a29a1026389d
aylestoneallotments <newsletter@aylestoneallotments.co.uk>
2016-04-30 09:11:55 +0100

    Move modernizr to front of js

--- js/application.js
+++ js/application.js
@@ -1,12 +1,12 @@
 ---
 ---
 
+{% include js/modernizr.js %}
 {% include bower_components/moment/min/moment.min.js %}
 {% include bower_components/fullcalendar/dist/fullcalendar.min.js %}
 {% include js/jquery.jqgoogleforms.min.js %}
 {% include bower_components/smoothstate/jquery.smoothState.min.js %}
 {% include bower_components/lunr.js/lunr.min.js %}
-{% include js/modernizr.js %}
 {% include js/scripts.js %}
 {% include js/lunrfeed.js %}
 {% include js/calendar.js %}
